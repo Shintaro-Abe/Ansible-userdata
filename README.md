7bed2811a6c452c39b9438d350e264de5071fac0
Shintaro Abe <121922274+Shintaro-Abe@users.noreply.github.com>
2023-05-19 04:32:38 +0900

    Fixing code

--- README.md
+++ README.md
@@ -8,9 +8,15 @@ amazon-linux-extrasを使用するため。
 以下の流れでインストールを実施し、インスタンスへ初めてログインした段階でAnsibleを使用できる環境を構築。
 * __Ansibleのインストール__
 * __RegionとOutoputの設定__
+* __ssh公開鍵と秘密鍵の生成__
 * __ansible.cfgの設定__
 * __ansibleディレクトリの作成__
 
 ## コマンドについて
 ユーザーデータはroot権限で実行する性質を持つため、ec2-userで実行したいコマンドには` sudo -u ec2-user -i `をつけて一時的にユーザーを変更。
 
+## playbookについて
+
+以下のリポジトリを参照。
+
+<a href="https://github.com/Shintaro-Abe/Ansible.git"><img src="https://img.shields.io/badge/ShintaroAbe/Ansible-181717.svg?logo=Ansible&style=flat-square"></a>
--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Region=ap-northeast-1
-Output=table
+Output=json
 
 amazon-linux-extras enable ansible2
 yum install -y ansible                       
@@ -9,6 +9,7 @@ yum install -y pip
 sudo -u ec2-user -i pip install --upgrade pip
 sudo -u ec2-user -i pip install boto
 yum install -y expect
+
 expect -c "
 spawn sudo -u ec2-user -i aws configure
 expect \"AWS Access Key ID : \"
@@ -22,9 +23,22 @@ send \"${Output}\n\"
 expect \"$\"
 exit 0
 "
+
+expect -c "
+spawn sudo -u ec2-user -i ssh-keygen -t rsa
+expect \"Enter file in which to save the key (/home/ec2-user/.ssh/id_rsa): \"
+send \"/home/ec2-user/.ssh/id_rsa\n\"
+expect \"Enter passphrase (empty for no passphrase):\"
+send \"\n\"
+expect \"Enter same passphrase again:\"
+send \"\n\"
+expect \"$\"
+exit 0
+"
+
 sed -i '12i interpreter_python = /usr/bin/python' /etc/ansible/ansible.cfg 
 sed -i '13i host_key_checking = False' /etc/ansible/ansible.cfg 
 sed -i '14i remote_user = ec2-user' /etc/ansible/ansible.cfg 
-sed -i '15i private_key_file = ~/.ssh/abetest.pem' /etc/ansible/ansible.cfg 
+sed -i '15i private_key_file = ~/.ssh/id_rsa_key' /etc/ansible/ansible.cfg 
 
 sudo -u ec2-user -i mkdir ansible
